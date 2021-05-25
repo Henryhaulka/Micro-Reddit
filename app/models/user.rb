bbd6d92318fa16c88922bd42cbded0b07738c3ac
henryhaulka <onuhenry24@gmail.com>
2021-05-25 17:19:30 +0100

    updare user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
     validates :username, presence: true, length: { minimum: 3}, uniqueness: true
     validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, on: :create } 
 end
