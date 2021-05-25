fd980737501b00c2354f4a9454e3372cf62bba83
henryhaulka <onuhenry24@gmail.com>
2021-05-25 15:42:23 +0100

    add User model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    validates :username, presence: true, length: { minimum: 3}, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, on: :create } 
+end
